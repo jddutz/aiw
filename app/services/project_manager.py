808939749ba02ffb1165c39cc6b84b12e93522a0
John Dutz <johnddutz@gmail.com>
2023-10-14 15:12:46 -0600

    added project_manager

---
+++ app/services/project_manager.py
@@ -0,0 +1,16 @@
+from app.models import WritingProject, db
+
+def create_new_project(project_info):
+    """
+    Create a new WritingProject based on the provided project_info.
+    
+    Args:
+    - project_info (dict): Dictionary containing the new project's details. 
+
+    Returns:
+    - (WritingProject): The newly created WritingProject instance.
+    """
+    new_project = WritingProject(title=project_info['title'], description=project_info['description'])
+    db.session.add(new_project)
+    db.session.commit()
+    return new_project
