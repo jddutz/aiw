dc6e8681c986407c6346ca66a0b1afc9e5200d05
John Dutz <johnddutz@gmail.com>
2023-10-14 10:12:54 -0600

    Updated app config

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://johndutz:password@localhost/johndutz$mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Avoid importing routes or models here
--- app/main.py
+++ app/main.py
@@ -1,4 +1,1 @@
 from app import app, db
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://johndutz:password@localhost/johndutz$mydatabase'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
