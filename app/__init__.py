f47bdc2f53f0d156b0fe12e3473f5657fb69b59c
John Dutz <johnddutz@gmail.com>
2023-10-14 10:18:43 -0600

    Updated SQLAlchemy database config

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://johndutz:password@localhost/johndutz$mydatabase'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="johndutz",
+    password="03132012",
+    hostname="johndutz.mysql.pythonanywhere-services.com",
+    databasename="johndutz$aiw",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 # Avoid importing routes or models here
