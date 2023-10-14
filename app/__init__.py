1a3150734c5801522cace20986385035e4531778
John Dutz <johnddutz@gmail.com>
2023-10-14 10:59:31 -0600

    Update db config

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ flask_app = Flask(__name__)
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="johndutz",
-    password="zm#gQh3&F!8BF6if",
+    password="zmgQh3F8BF6if",
     hostname="johndutz.mysql.pythonanywhere-services.com",
     databasename="johndutz$aiw",
 )
