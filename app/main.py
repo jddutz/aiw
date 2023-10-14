e1b7e9d6e9043e63e5be80665e4b7448e50f86f6
John Dutz <johnddutz@gmail.com>
2023-10-14 10:11:30 -0600

    Update mysql config

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 from app import app, db
-#from flask_migrate import Migrate
 
-#migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://johndutz:password@localhost/johndutz$mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/johndutz$aiw' # 'sqlite:///site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
