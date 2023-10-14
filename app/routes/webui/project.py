0f156218eee60236200f77580e46e16f83e1d316
John Dutz <johnddutz@gmail.com>
2023-10-14 15:13:02 -0600

    Updated create_project endpoint

--- app/routes/webui/project.py
+++ app/routes/webui/project.py
@@ -1,16 +1,26 @@
 # app.routes.project_routes.py
 
-from flask import render_template, redirect, url_for, request, Blueprint
-from app.models import WritingProject  # Assuming this is your project model
+from flask import render_template, redirect, url_for, request, Blueprint, flash
+from app.models import WritingProject
+from app.services import project_manager
 
 project_blueprint = Blueprint('project', __name__)
 
 @project_blueprint.route('/create', methods=['GET', 'POST'])
 def create_project():
     if request.method == 'POST':
-        # Logic to create a new project goes here
-        pass
-    return render_template('create_project.html')  # The UI for creating a project
+        project_info = {
+            "title": request.form.get("title"),
+            "description": request.form.get("description")
+        }
+        try:
+            new_project = project_manager.create_new_project(project_info)
+            flash("Project successfully created!", "success")
+            return redirect(url_for('project.project_detail', project_id=new_project.id))
+        except Exception as e:
+            flash(str(e), "danger")
+            return render_template('create_project.html')
+    return render_template('create_project.html')
 
 @project_blueprint.route('/', methods=['GET'])
 def list_projects():
